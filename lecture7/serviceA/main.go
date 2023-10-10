d9c5a2a0bcc9ee0b7a7349b1f02b90a7962ed5e8
ysultanov <erkanatsultanov1999@gmail.com>
2023-10-10 12:41:31 +0600

    lecture 7 home task

--- lecture7/serviceA/main.go
+++ lecture7/serviceA/main.go
@@ -1,1 +1,22 @@
-package serviceA
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/get_data_from_service_b", func(w http.ResponseWriter, r *http.Request) {
+		resp, err := http.Get("http://localhost:8081/send_data_to_service_a")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer resp.Body.Close()
+		data, _ := ioutil.ReadAll(resp.Body)
+		fmt.Fprintf(w, "Data from service B: %s", data)
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
--- lecture7/serviceB/main.go
+++ lecture7/serviceB/main.go
@@ -1,1 +1,16 @@
-package serviceB
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/send_data_to_service_a", func(w http.ResponseWriter, r *http.Request) {
+		// You can customize the data to be sent to service A here.
+		data := "Hello from service B"
+		fmt.Fprintf(w, "Data to service A: %s", data)
+	})
+
+	http.ListenAndServe(":8081", nil)
+}
