cd54d0c4cd0ccb4db54e9e5c80f2acda5d183624
ysultanov <erkanatsultanov1999@gmail.com>
2023-10-10 12:51:10 +0600

    lecture 7 hw

--- lecture7/serviceA/main.go
+++ lecture7/serviceA/main.go
@@ -7,15 +7,15 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/get_data_from_service_b", func(w http.ResponseWriter, r *http.Request) {
-		resp, err := http.Get("http://localhost:8081/send_data_to_service_a")
+	http.HandleFunc("/get_serviceB", func(w http.ResponseWriter, r *http.Request) {
+		resp, err := http.Get("http://localhost:8081/sending_to_serviceA")
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 		defer resp.Body.Close()
 		data, _ := ioutil.ReadAll(resp.Body)
-		fmt.Fprintf(w, "Data from service B: %s", data)
+		fmt.Fprintf(w, "Info from B: %s", data)
 	})
 
 	http.ListenAndServe(":8080", nil)
--- lecture7/serviceB/main.go
+++ lecture7/serviceB/main.go
@@ -6,10 +6,10 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/send_data_to_service_a", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/sending_to_serviceA", func(w http.ResponseWriter, r *http.Request) {
 		// You can customize the data to be sent to service A here.
 		data := "Hello from service B"
-		fmt.Fprintf(w, "Data to service A: %s", data)
+		fmt.Fprintf(w, "Info TO service A: %s", data)
 	})
 
 	http.ListenAndServe(":8081", nil)
